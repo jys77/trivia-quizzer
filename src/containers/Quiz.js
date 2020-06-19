25843bdfc6d07e83ce8d5cec549596fb250c0de1
jys77 <jiang-yunsheng@hotmail.com>
2020-06-19 14:05:49 -0400

    fix: quiz layout

--- src/containers/Quiz.js
+++ src/containers/Quiz.js
@@ -9,18 +9,56 @@ const QuizWrapper = styled.div`
   transform: translate(-50%, -50%);
   display: flex;
   flex-flow: column;
-  .question {
+  align-items: center;
+  @media (max-width: 767px) {
+    width: 90%;
+  }
+  .question-outer {
     width: 600px;
     height: 100px;
     border: 1px white solid;
     border-radius: 10px;
     background-color: #fff;
+    font-size: 1.5rem;
+    display: table;
+    padding: 10px;
+    @media (max-width: 767px) {
+      width: 100%;
+    }
+    .question-inner {
+      display: table-cell;
+      vertical-align: middle;
+      text-align: center;
+      width: 100%;
+    }
   }
   .options {
     display: flex;
     flex-wrap: wrap;
-    width: 100%;
-    .option {
+    width: 600px;
+    justify-content: space-between;
+    font-size: 1.5rem;
+    @media (max-width: 767px) {
+      width: 100%;
+    }
+    .option-outer {
+      margin-top: 2rem;
+      border: 1px solid white;
+      border-radius: 10px;
+      background-color: #fff;
+      height: 50px;
+      padding: 5px;
+      width: 290px;
+      display: table;
+      @media (max-width: 767px) {
+        width: 100%;
+      }
+      .option-inner {
+        display: table-cell;
+        vertical-align: middle;
+        text-align: center;
+        width: 100%;
+      }
     }
   }
 `;
@@ -40,16 +78,20 @@ export const Quiz = ({ questions }) => {
 
   return questions.length > 0 ? (
     <QuizWrapper>
-      <div
-        className="question"
-        dangerouslySetInnerHTML={{ __html: questions[0].question }}
-      ></div>
+      <div className="question-outer">
+        <div
+          className="question-inner"
+          dangerouslySetInnerHTML={{ __html: questions[0].question }}
+        ></div>
+      </div>
       <div className="options">
         {options.map((opt) => (
-          <div
-            className="option"
-            dangerouslySetInnerHTML={{ __html: opt }}
-          ></div>
+          <div className="option-outer">
+            <div
+              className="option-inner"
+              dangerouslySetInnerHTML={{ __html: opt }}
+            ></div>
+          </div>
         ))}
       </div>
     </QuizWrapper>
