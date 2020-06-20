05cad30ac4b72d91875eb1831a4d65521eb19630
jys77 <jiang-yunsheng@hotmail.com>
2020-06-20 16:31:17 -0400

    fix: result buttons responsive adjustment

--- src/containers/Result.js
+++ src/containers/Result.js
@@ -54,6 +54,9 @@ const ResultWrapper = styled.div`
       transition: all 0.2s ease-in-out;
       &:hover {
         transform: scale(1.1);
+        @media (max-width: 767px) {
+          transform: scale(1);
+        }
       }
       @media (max-width: 767px) {
         width: 100%;
